a9ec082eab77e3cbb5e0685bae28ad277582566a
mark-nader <36566830+mark-nader@users.noreply.github.com>
2018-11-19 19:38:28 +0000

    Add files via upload

--- bassNoteHyperParam.py
+++ bassNoteHyperParam.py
@@ -3,16 +3,13 @@ import NeuralNetUtils as nnu
 import MidiUtils as mu
 import random
 
+resumeTraining=True
+nameToSave="bassNotes.pt"
 dimIn, dimHidden, dimOut, hiddenLayers = 12, 64, 12, 6
-
 trainSize, testSize = 1, 1
-
 learningRate, weightDecay = 0.001, 0.001
-
 repeatWeight, fifthWeight = 0.5, 0.5
-
 sequenceLength = 8
-
 numEpochs=10
 testEvery=2
 
@@ -25,6 +22,14 @@ model=nnu.LSTM_LogSoftMax_RNN(dimIn,dimHidden,dimOut,hiddenLayers,cuda)
 model=model.to(cuda)		######### GPU
 lossFunction=torch.nn.NLLLoss(weight=torch.tensor([repeatWeight,1,1,1,1,1,1,fifthWeight,1,1,1,1]).to(cuda))
 optimizer=torch.optim.SGD(model.parameters(), lr=learningRate, weight_decay=weightDecay)
+
+prevEpochs=0
+if resumeTraining:
+	checkpoint = torch.load(nameToSave)
+	model.load_state_dict(checkpoint['model_state_dict'])
+	optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+	prevEpochs = checkpoint['epoch']
+	modelG.train()
 
 songList=mu.getSongList("projectMidiTraining")
 random.shuffle(songList)
@@ -101,7 +106,11 @@ while epoch <= numEpochs:
 		epoch+=1
 		printedTestLoss=False
 
-torch.save(model.state_dict(), "bassNotes.pt")
+torch.save({
+            'epoch': prevEpochs+numEpochs,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            }, nameToSave)
 
 print("################################################")
 print("Input dimension = {}, Hidden dimension ={}, Output dimension = {}, Hidden layers = {}".format(dimIn,dimHidden,dimOut,hiddenLayers))
